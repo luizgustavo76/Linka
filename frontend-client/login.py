a6526dd3d617d9b350644d6d51f3438ac869dce2
luizsgustavo76 <luizsgustavo76@gmail.com>
2026-03-27 19:36:38 -0300

    login com interface minima de entrar

--- frontend-client/login.py
+++ frontend-client/login.py
@@ -1,4 +1,5 @@
 import sys
+import requests
 from PyQt6.QtWidgets import (
     QApplication, QWidget, QLabel, QPushButton,
     QVBoxLayout, QHBoxLayout, QFrame,
@@ -13,13 +14,20 @@ def button(texto,  acao, janela):
     botao = QPushButton(texto, janela)
     botao.clicked.connect(acao)
     layout.addWidget(botao)
+def pegar_texto(entrada):
+    texto = entrada.text()
 def entrar():
-    print("butão")
+    entrada_usuario("Nome de usuário")
+    entrada_usuario("senha")
+    enviar = button("Pronto", lambda: pegar_texto(enviar), window)
 def cadastro():
     pass
+def entrada_usuario(texto):
 
+    entrada = QLineEdit()
+    entrada.setPlaceholderText(texto)
+    layout.addWidget(entrada)
 def main():
-
 
     
     window.setWindowTitle("Faça login no Linka!")
