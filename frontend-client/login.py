93f118ae232d0c33e8b670ebcd4f8d4380d324b1
luizsgustavo76 <luizsgustavo76@gmail.com>
2026-03-27 13:08:21 -0300

    escopo minimo de login em pyqt6

--- frontend-client/login.py
+++ frontend-client/login.py
@@ -1,15 +1,32 @@
 import sys
-from PyQt6.QtWidgets import QApplication, QWidget
+from PyQt6.QtWidgets import (
+    QApplication, QWidget, QLabel, QPushButton,
+    QVBoxLayout, QHBoxLayout, QFrame,
+    QScrollArea, QLineEdit
+)
+from PyQt6.QtCore import Qt, QTimer, pyqtSignal, QObject
+from PyQt6.QtWidgets import QSizePolicy
+app = QApplication(sys.argv)
+window = QWidget()
+layout = QVBoxLayout(window)
+def button(texto,  acao, janela):
+    botao = QPushButton(texto, janela)
+    botao.clicked.connect(acao)
+    layout.addWidget(botao)
+def entrar():
+    print("butão")
+def cadastro():
+    pass
 
 def main():
-    # Cria a aplicação (obrigatório)
-    app = QApplication(sys.argv)
 
-    # Cria a janela principal
-    window = QWidget()
+
+    
     window.setWindowTitle("Faça login no Linka!")
     window.resize(400, 300) 
     window.show()
+    button("Entrar", entrar, window)
+    button("cadastrar-se", cadastro, window)
     sys.exit(app.exec())
 
 if __name__ == "__main__":
