0c8a9bb2a806923796c78d5c1fd734a339e74d92
LUIZ GUSTAVO CUSTÓDIO FERREIRA LOURENÇO <luiz_g_lourenco@estudante.sesisenai.org.br>
2026-03-30 13:41:58 -0300

    ferramenta de criação de app com pyqt pra ser mais facilitada

--- frontend-client/QTools.py
+++ frontend-client/QTools.py
@@ -5,3 +5,34 @@ from PyQt6.QtWidgets import (
 )
 from PyQt6.QtCore import Qt, QTimer, pyqtSignal, QObject
 from PyQt6.QtWidgets import QSizePolicy
+import sys
+import os
+app = QApplication(sys.argv)
+window = QWidget()
+layout = QVBoxLayout(window)
+def user_entry(texto):
+    entrada = QLineEdit()
+    entrada.setPlaceholderText(texto)
+    layout.addWidget(entrada)
+    return entrada
+def get_text(entrada):
+    texto = entrada.text()
+    return texto
+def label(text, window):
+    label_text = QLabel(text)
+
+    if window.layout() is None:
+        layout = QVBoxLayout()
+        window.setLayout(layout)
+    else:
+        layout = window.layout()
+def button(texto,  acao, janela):
+    if acao == "None":
+        botao = QPushButton(texto, janela)
+        layout.addWidget(botao)
+        return botao
+    else:
+        botao = QPushButton(texto, janela)
+        botao.clicked.connect(acao)
+        layout.addWidget(botao)
+        return botao
