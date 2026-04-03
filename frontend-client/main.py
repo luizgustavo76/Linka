9a4e2995db4d9196cbed2f7f024f21808bf3cc72
luizsgustavo76 <luizsgustavo76@gmail.com>
2026-04-03 17:52:11 -0300

    deploy antes de sair

--- frontend-client/main.py
+++ frontend-client/main.py
@@ -1,7 +1,13 @@
 import login
 import menu
 import os
+import configparser
 if not os.path.exists("config-login.cfg"):
     login.main_app()
 else:
-    menu.main_app()
+    config = configparser.ConfigParser()
+    config.read("config-login.cfg")
+    if config["FAST-LOGIN"]["username"] == "" or config["FAST-LOGIN"]["password"] == "":
+        login.main_app()
+    else:
+        menu.main_app()
