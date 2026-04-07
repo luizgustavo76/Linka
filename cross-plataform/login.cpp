e470718e2b7f0d727e4f11d1ff88ffe95713b2d3
luizsgustavo76 <luizsgustavo76@gmail.com>
2026-04-07 20:23:20 -0300

    tentativa falhada de tentar usar qt em c++(muito dificil)

--- cross-plataform/login.cpp
+++ cross-plataform/login.cpp
@@ -1,9 +1,18 @@
 #include <QApplication>
 #include <QTextEdit>
-
+#include <iostream>
+#include <QtWidgets>
+#include <QtCore>
+#include <QtGui>
+using namespace std;
 int main(int argv, char **args)
 {
- QApplication app(argv, args);
+QApplication app(argv, args);
+QWidget window;
+window.setWindowTitle("Linka Mobile");
+window.resize(400, 600);
 
+QPushButton button("HI!");
+button.show();
 return app.exec();
 }
