53d770bdad47aee34efc1065d5f3c73c6909f453
luizsgustavo76 <luizsgustavo76@gmail.com>
2026-04-11 16:26:55 -0300

    header de tradutor c++

---
+++ cross-plataform/translator.h
@@ -0,0 +1,10 @@
+#ifndef TRANSLATOR_H
+#define TRANSLATOR_H
+#include <nlohmann/json.hpp>
+std::ifstream f("example.json");
+using json = nlohmann::json;
+int translate()
+{
+    return 0;
+}
+#endif
