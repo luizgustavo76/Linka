238fa1eb7058ce47bc5336338592d8fffbf53a34
luizsgustavo76 <luizsgustavo76@gmail.com>
2026-04-12 16:37:04 -0300

    login c++ com funções pra facilitar desenvolvimento

--- cross-plataform/login.cpp
+++ cross-plataform/login.cpp
@@ -1,19 +1,39 @@
 #include <QApplication>
 #include <QWidget>
 #include <QPushButton>
-
+#include <stdio.h>
+#include <QVBoxLayout>
+#include "translator.h"
+#include <QLineEdit>
+#include <QDebug>
 int main(int argc, char *argv[])
 {
     QApplication app(argc, argv);
-
+    QString signup_text = QString::fromStdString(translate("initial-page", "sign-up"));
     QWidget window;
     window.setWindowTitle("Linka Mobile");
     window.resize(400, 600);
 
-    QPushButton button("HI!", &window);
-    button.move(150, 250);
+    QVBoxLayout *layout = new QVBoxLayout(&window);
+    auto entry = [&](QString text){
+        QLineEdit *input = new QLineEdit();
+        input->setPlaceholderText(text);
+        layout->addWidget(input);
+    };
+    auto signup = [&]()
+    {
+        qDebug() << "CLICOU!";
+        entry("hi");
+    };
+    auto button = [&](QString text, std::function<void()> func)
+    {
+        QPushButton *btn = new QPushButton(text);
+        layout->addWidget(btn);
+
+        QObject::connect(btn, &QPushButton::clicked, func);
+    };
+    button(signup_text, signup);
 
     window.show();
-
     return app.exec();
 }
