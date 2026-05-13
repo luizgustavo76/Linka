4584f95b231c7e2cebbcd4d17999e262e945b84f
luizsgustavo76 <luizsgustavo76@gmail.com>
2026-05-13 20:05:43 -0300

    linka lite com uma versão minima no site principal

--- docs/login.js
+++ docs/login.js
@@ -1,48 +1,26 @@
-async function login_request() {
-    const username = document.getElementById("user").value;
-    const password = document.getElementById("pass").value;
+function login_request() {
+    var username = document.getElementById("user").value;
+    var password = document.getElementById("pass").value;
 
-    const resposta = await fetch("http://127.0.0.1:5000/login", {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json"
-        },
-        body: JSON.stringify({
-            username,
-            password
-        })
+    var xhr = new XMLHttpRequest();
+    xhr.open("POST", "http://linkaProject.pythonanywhere.com/login", true);
+    xhr.setRequestHeader("Content-Type", "application/json");
+
+    xhr.onreadystatechange = function () {
+        if (xhr.readyState === 4) {
+            if (xhr.status === 200) {
+                var data = JSON.parse(xhr.responseText);
+                alert("Login feito com sucesso");
+            } else {
+                alert("Falha no login");
+            }
+        }
+    };
+
+    var body = JSON.stringify({
+        username: username,
+        password: password
     });
 
-    const data = await resposta.json();
-
-    if (resposta.status == 200) {
-        
-        alert("Login feito com sucesso");
-    } else {
-        alert("Falha no login");
-    }
-}
-async function register_request() {
-    const username = document.getElementById("reg-user").value;
-    const password = document.getElementById("reg-pass").value;
-    const email = document.getElementById("reg-email").value;
-    const resposta = await fetch("http://127.0.0.1:5000/register", {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json"
-        },
-        body: JSON.stringify({
-            username,
-            password,
-            email
-        })
-    });
-
-    const data = await resposta.json();
-
-    if (resposta.status in (200, 201)) {
-        alert("conta feita com sucesso");
-    } else {
-        alert("Falha no login");
-    }
+    xhr.send(body);
 }
