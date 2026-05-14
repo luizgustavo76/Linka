38e9ccee3113ab8312d113a511c210d35ab228ee
luizsgustavo76 <luizsgustavo76@gmail.com>
2026-05-14 20:53:29 -0300

    login funcionando e fazendo request faltando salvar localmente os dados de login

--- LinkaLite/app/src/main/assets/login.js
+++ LinkaLite/app/src/main/assets/login.js
@@ -1,26 +1,5 @@
 function login_request() {
-    var username = document.getElementById("user").value;
-    var password = document.getElementById("pass").value;
-
-    var xhr = new XMLHttpRequest();
-    xhr.open("POST", "http://linkaProject.pythonanywhere.com/login", true);
-    xhr.setRequestHeader("Content-Type", "application/json");
-
-    xhr.onreadystatechange = function () {
-        if (xhr.readyState === 4) {
-            if (xhr.status === 200) {
-                var data = JSON.parse(xhr.responseText);
-                alert("Login feito com sucesso");
-            } else {
-                alert("Falha no login");
-            }
-        }
-    };
-
-    var body = JSON.stringify({
-        username: username,
-        password: password
-    });
-
-    xhr.send(body);
+    var username = document.getElementById("username").value;
+    var password = document.getElementById("password").value;
+    linka.httpGet("http://linkaProject.pythonanywhere.com/login?username=" + username + "?password=" + password);
 }
