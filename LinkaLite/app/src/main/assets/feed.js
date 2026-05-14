0281f94a9237c0d2e24788d0a73bf3a57abae805
LUIZ GUSTAVO CUSTÓDIO FERREIRA LOURENÇO <luiz_g_lourenco@estudante.sesisenai.org.br>
2026-05-14 13:18:50 -0300

    correções bem pequenas no linkalite

--- LinkaLite/app/src/main/assets/feed.js
+++ LinkaLite/app/src/main/assets/feed.js
@@ -1,14 +1,16 @@
 function feed(){
-    Linka.httpGet("http://linkaProject.pythonanywhere.com/feed");
-    document.getElementById("saida").innerHTML = "RESPOSTA:\n" + txt;
-    let obj = JSON.parse(txt);
-    let html = "";
-    for (let i = 0; i < obj.length; i++) {
-        html +=
-            "<div class='post'>" +
-            "<b>" + obj[i].user + "</b><br>" +
-            obj[i].text +
-            "</div><hr>";
-    }
-    document.getElementById("saida").innerHTML = html;
+    Linka.httpGet("http://linkaProject.pythonanywhere.com/feed", function(txt) {
+        let obj = JSON.parse(txt);
+
+        let html = "";
+        for (let i = 0; i < obj.length; i++) {
+            html +=
+                "<div class='post'>" +
+                "<b>" + obj[i].user + "</b><br>" +
+                obj[i].text +
+                "</div><hr>";
+        }
+
+        document.getElementById("saida").innerHTML = html;
+    });
 }
