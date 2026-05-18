a9a79f1b06066f392226b638187e5543a3968fd0
LUIZ GUSTAVO CUSTÓDIO FERREIRA LOURENÇO <luiz_g_lourenco@estudante.sesisenai.org.br>
2026-05-18 14:52:56 -0300

    adição de stars no feed do linkaLite

--- LinkaLite/app/src/main/assets/feed.js
+++ LinkaLite/app/src/main/assets/feed.js
@@ -1,8 +1,20 @@
-function feed(){
-    document.getElementById("debug").innerHTML = "Chamando feed...";
-    Linka.httpGet("http://linkaProject.pythonanywhere.com/feed");
-}
+var url = "http://linkaProject.pythonanywhere.com";
 
+    if (cfg["SERVER"] && cfg["SERVER"]["url"]) {
+        url = cfg["SERVER"]["url"];
+    }
+function feed(){
+    document.getElementById("debug").innerHTML = "Loading feed...";
+    Linka.httpGet(url + "/feed");
+}
+function requestStar(id, username){
+    json_star = {
+        "username":username,
+        "post_id":id
+    };
+    Linka.httpPost(url + "/star", JSON.stringify(json_star));
+    
+}
 function receberResposta(txt){
     document.getElementById("debug").innerText = txt;
 
@@ -13,7 +25,7 @@ function receberResposta(txt){
     for (let i = 0; i < obj.length; i++) {
         html += "<div class='post'>" +
             "<b>" + obj[i].username + "</b><br>" +
-            obj[i].text_post +
+            obj[i].text_post + "<a onclick='requestStar(" + obj[i].id + ", \"" + obj[i].username + "\")'>Star</a>"
             "</div><hr>";
     }
 
