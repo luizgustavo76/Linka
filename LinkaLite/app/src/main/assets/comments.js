6a58e5966094f8a59725f038b726be4f740ad746
luizsgustavo76 <luizsgustavo76@gmail.com>
2026-05-20 14:56:28 -0300

    comments.js funcionando fazendo o request pro servidor e pegando os comentarios dos posts

--- LinkaLite/app/src/main/assets/comments.js
+++ LinkaLite/app/src/main/assets/comments.js
@@ -1,3 +1,14 @@
-document.addEventListener("DOMContentLoaded", () => {
-    iniciar();
-});
+function request_comments(post_id){
+    json_comment = {"post_id":post_id}
+    Linka.httpPost(url + "/view-comments");
+    var obj = JSON.parse(txt);
+    const comments = obj["comments"];
+    for (var i = 0; i < comments.length; i++){
+        var html = "";
+        var username = comments[i]["username"];
+        var text_comment = comments[i]["text_comment"];
+        var comment_id = comments[i]["comment_id"];
+        html += '<div class="post">' + '<b>' + username + "</b>" +"<br>" + "<p>" + text_comment + "</p><br>";
+        document.getElementById("commments").innerHTML = html;
+    }
+}
