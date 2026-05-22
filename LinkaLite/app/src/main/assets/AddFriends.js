9f57d1883f15bf3294bf4d7755c2923370a02dae
luizsgustavo76 <luizsgustavo76@gmail.com>
2026-05-21 21:20:00 -0300

    sender no request ao adicionar uma amizade no cliente lite

--- LinkaLite/app/src/main/assets/AddFriends.js
+++ LinkaLite/app/src/main/assets/AddFriends.js
@@ -1,4 +1,16 @@
 function request_add_friends(){
+    var cfg = JSON.parse(Linka.loadCfgAsJson("config-login.cfg"));
+    var url = "http://linkaProject.pythonanywhere.com";
+    if (cfg["SERVER"] && cfg["SERVER"]["url"]) {
+        url = cfg["SERVER"]["url"];
+    };
+    var sender = cfg["FAST-LOGIN"]["username"];
     var username = document.getElementById("username").value;
-    var message = document.getElementById("message").value;  
+    var message = document.getElementById("message").value;
+    json_friends = {
+        "receiver":username,
+        "message":message,
+        "sender":sender
+    };
+    Linka.httpPost(url + "/feed", JSON.stringify(json_friends));
 };
--- LinkaLite/app/src/main/java/com/LinkaProject/linkaLite/MainActivity.java
+++ LinkaLite/app/src/main/java/com/LinkaProject/linkaLite/MainActivity.java
@@ -211,13 +211,13 @@ public class MainActivity extends Activity {
 
   
         @JavascriptInterface
-        public saveCfg(String filename, String content) {
+        public String saveCfg(String filename, String content) {
             try {
                 OutputStreamWriter writer =
                         new OutputStreamWriter(openFileOutput(filename, MODE_PRIVATE));
                 writer.write(content);
                 writer.close();
-                return "sucessful!";
+                return "successful!";
             } catch (Exception e) {
                 e.printStackTrace();
                 return e.toString();
