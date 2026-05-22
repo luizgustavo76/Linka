b069c5637e2eaa96ee207ead446fcad8ba60f27f
luizsgustavo76 <luizsgustavo76@gmail.com>
2026-05-21 21:27:18 -0300

    bridge java corrigido pra salvar as configurações no local do linka

--- LinkaLite/app/src/main/assets/login.js
+++ LinkaLite/app/src/main/assets/login.js
@@ -4,6 +4,7 @@ var statusCode = 0;
 function receberStatusCode(code){
     statusCode = code;
 }
+
 function receberResposta(txt){
     lastResponse = txt;
     document.getElementById("saida").innerHTML = txt;
@@ -35,25 +36,37 @@ function login_request() {
 
     setTimeout(function(){
 
-        if (!lastResponse){
+        var response = lastResponse;
+        var code = statusCode;
+
+        if (!response){
             document.getElementById("saida").innerHTML = "error in the server side";
             return;
         }
 
-        var obj = JSON.parse(lastResponse);
+        var obj = JSON.parse(response);
 
-        if (statusCode == 200 || statusCode == 201){
+        if (code == 200 || code == 201){
 
             Linka.httpPost(url + "/new-session", JSON.stringify(login_json));
 
             setTimeout(function(){
 
-                var sessionObj = JSON.parse(lastResponse);
+                var sessionResponse = lastResponse;
+
+                if (!sessionResponse){
+                    document.getElementById("saida").innerHTML = "error in session";
+                    return;
+                }
+
+                var sessionObj = JSON.parse(sessionResponse);
 
                 var token = sessionObj.token;
 
                 var content = "";
-                content += "[FAST-LOGIN]\n";
+                content += "[SERVER]\n";
+                content += "url=" + url + "\n\n";
+                content += "[LOGIN]\n";
                 content += "username=" + username + "\n";
                 content += "token_session=" + token + "\n";
 
@@ -70,43 +83,57 @@ function login_request() {
 
     }, 600);
 }
+
+
 function register_request(){
+
     var username = document.getElementById("reg-user").value;
     var password = document.getElementById("reg-pass").value;
     var email = document.getElementById("reg-email").value;
+
     var request_json = {
-        "username":username,
-        "password":password,
-        "email":email
+        "username": username,
+        "password": password,
+        "email": email
     };
+
     var cfg = JSON.parse(Linka.loadCfgAsJson("config-login.cfg"));
+
     var url = "http://linkaProject.pythonanywhere.com";
 
     if (cfg["SERVER"] && cfg["SERVER"]["url"]) {
         url = cfg["SERVER"]["url"];
     }
-    Linka.httpPost(url + "/register", JSON.stringify(request_json))
+
+    Linka.httpPost(url + "/register", JSON.stringify(request_json));
 
     setTimeout(function(){
 
-        if (!lastResponse){
+        var response = lastResponse;
+        var code = statusCode;
+
+        if (!response){
             document.getElementById("saida").innerHTML = "error in the server side";
             return;
         }
 
-        var obj = JSON.parse(lastResponse);
-        if (statusCode == 200 || statusCode == 201){
+        var obj = JSON.parse(response);
+
+        if (code == 200 || code == 201){
+
             var content = "";
             content += "[FAST-LOGIN]\n";
-            content += "username = " + username;
-            content += "password = " + password;
-            
+            content += "username=" + username + "\n";
+            content += "password=" + password + "\n";
+
+            Linka.saveCfg("config-login.cfg", content);
+
             window.location.href = "file:///android_asset/index.html";
-        }else{
+
+        } else {
             document.getElementById("saida").innerHTML =
                 "<h3>The username or password is invalid</h3>";
         }
-    }, 600)
 
-
+    }, 600);
 }
