8666cac9b3397097ce79361f6dc307d37970000b
luizsgustavo76 <luizsgustavo76@gmail.com>
2026-05-22 18:16:52 -0300

    placeholder de backend de notificações, futuramente criar um polling em android pra receber notificações sem usar notificações push para ter compatibilidade

--- LinkaLite/app/src/main/assets/inbox.js
+++ LinkaLite/app/src/main/assets/inbox.js
@@ -1,23 +1,23 @@
-function receberStatusCode(code){
-    statusCode = code;
-}
-
-function receberResposta(txt){
-    lastResponse = txt;
-    document.getElementById("saida").innerHTML = txt;
-}
-
-function receberErro(txt){
-    document.getElementById("saida").innerHTML = "ERRO: " + txt;
-}
-
 function request_inbox(){
-    var cfg = Linka.loadCfgAsJson("config-login.cfg");
-    var username = cfg["FAST-LOGIN"]["username"];
-    var url = cfg["SERVER"]["url"];
-    json_inbox = {
-        "username":username
-    };
-    Linka.httpPost(url + "/inbox", json_inbox);
 
+    var cfg = JSON.parse(
+        Linka.loadCfgAsJson("config-login.cfg")
+    );
+
+    var username = cfg["FAST-LOGIN"]["username"];
+
+    var url = "http://linkaProject.pythonanywhere.com";
+
+    if (cfg["SERVER"] && cfg["SERVER"]["url"]) {
+        url = cfg["SERVER"]["url"];
+    }
+
+    var json_inbox = {
+        "username": username
+    };
+
+    Linka.httpPost(
+        url + "/inbox",
+        JSON.stringify(json_inbox)
+    );
 }
---
+++ backend/notifications.py
@@ -0,0 +1,3 @@
+from flask import Flask, jsonify, request, Blueprint
+import os
+import sqlite3
