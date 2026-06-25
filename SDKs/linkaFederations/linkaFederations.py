0010f41b69590e710eb048623ea765bf01f52eb9
LUIZ GUSTAVO CUSTÓDIO FERREIRA LOURENÇO <luiz_g_lourenco@estudante.sesisenai.org.br>
2026-06-25 13:39:02 -0300

    servidor com rotas corrigidas e linkaFederations ja fazendo requests

--- SDKs/linkaFederations/linkaFederations.py
+++ SDKs/linkaFederations/linkaFederations.py
@@ -28,3 +28,27 @@ class LinkaFederations:
     def sendPayload(payload, instance):
         if slugs_pre_loaded[instance]:
             url_instance = slugs_pre_loaded[instance]["url"]
+            response = requests.post(
+                url_instance,
+                json=payload,
+                timeout=10
+            )
+            return response
+        else:
+            response = request.get(url=https://linkaApi.pythoanywhere.com/meta)
+            json_response = json_load(response)
+            url_gate = json_response["url"]["linkaGate"]
+            response_slug = request.post(
+                url_gate + "/consult",
+                json={'name_server':instance},
+                timeout=10
+            )
+            if response_slug.status_code == "200":
+                url = response_slug["url"]
+                response = requests.post(
+                    url_instance,
+                    json=payload,
+                    timeout=10
+                )
+                return response
+
