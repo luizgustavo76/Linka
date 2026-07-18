958cef530e3ae5e2e22067c60ca5f5d87f17464b
luizsgustavo76 <luizsgustavo76@gmail.com>
2026-07-18 19:35:21 -0300

    tela de cadastro abrindo mas sem função ainda

--- LinkaLite/app/src/main/java/com/LinkaProject/linkaLite/SignupActivity.java
+++ LinkaLite/app/src/main/java/com/LinkaProject/linkaLite/SignupActivity.java
@@ -1,9 +1,23 @@
 package com.LinkaProject.linkaLite;
-
+import com.LinkaProject.linkaLite.R;
 import android.app.Activity;
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
 public class SignupActivity extends Activity {
     private EditText edtUsername;
     private EditText edtPassword;
