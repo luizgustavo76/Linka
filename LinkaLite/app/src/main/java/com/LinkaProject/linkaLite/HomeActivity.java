c70b239491883dad20dd8f67cee432c4f406950e
luizsgustavo76 <luizsgustavo76@gmail.com>
2026-07-18 21:21:03 -0300

    tela de cadastro totalmente funcional e placeholder bem ruinzinho de home

--- LinkaLite/app/src/main/java/com/LinkaProject/linkaLite/HomeActivity.java
+++ LinkaLite/app/src/main/java/com/LinkaProject/linkaLite/HomeActivity.java
@@ -1,12 +1,35 @@
 package com.LinkaProject.linkaLite;
-
+import com.LinkaProject.linkaLite.R;
 import android.app.Activity;
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
 public class HomeActivity extends Activity {
+    private Button btnHome;
+    private Button btnProfile;
+    private Button btnOptions;
+    private Button btnChat;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        // Temporário só para compilar
+        setContentView(R.layout.activity_home);
+        btnHome = (Button) findViewById(R.id.btnHome);
+        btnChat = (Button) findViewById(R.id.btnChat);
+        btnProfile = (Button) findViewById(R.id.btnProfile);
+        btnOptions = (Button) findViewById(R.id.btnOptions);
     }
 }
--- LinkaLite/app/src/main/java/com/LinkaProject/linkaLite/SignupActivity.java
+++ LinkaLite/app/src/main/java/com/LinkaProject/linkaLite/SignupActivity.java
@@ -1,18 +1,14 @@
 package com.LinkaProject.linkaLite;
 import com.LinkaProject.linkaLite.R;
 import android.app.Activity;
-import android.app.ProgressDialog;
 import android.content.Intent;
-import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
-
 import org.json.JSONObject;
-
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
@@ -24,8 +20,52 @@ public class SignupActivity extends Activity {
     private EditText edtRetypePassword;
     private EditText edtEmail;
     private TextView txtGoToSignin;
+    private Button btnRegister;
+    public String requestHTTP(String urlParam, String method, JSONObject json_body) {
+        HttpURLConnection connection = null;
+        try {
+            URL url = new URL(urlParam);
+            connection = (HttpURLConnection) url.openConnection();
+            
+            method = method.toLowerCase();
+            if (method.equals("post")) {
+                connection.setRequestMethod("POST");
+            } else if (method.equals("get")) {
+                connection.setRequestMethod("GET");
+            }
+            
+            connection.setRequestProperty("Content-Type", "application/json");
+            connection.setDoOutput(true);
+
+            OutputStream os = connection.getOutputStream();
+            os.write(json_body.toString().getBytes("UTF-8"));
+            os.flush();
+            os.close();
+
+            int responseCode = connection.getResponseCode();
+            if (responseCode == HttpURLConnection.HTTP_OK) {
+                BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+                StringBuilder response = new StringBuilder();
+                String line;
+                while ((line = in.readLine()) != null) {
+                    response.append(line);
+                }
+                in.close();
+                return response.toString();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            if (connection != null) {
+                connection.disconnect();
+            }
+        }
+        return ""; 
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        final String baseUrl = "http://linkaProject.pythonanywhere.com";
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
         edtUsername = (EditText) findViewById(R.id.edtUsername);
@@ -33,5 +73,35 @@ public class SignupActivity extends Activity {
         edtRetypePassword = (EditText) findViewById(R.id.edtRetypePassword);
         edtEmail = (EditText) findViewById(R.id.edtEmail);
         txtGoToSignin = (TextView) findViewById(R.id.txtGoToSignin);
+        btnRegister = (Button) findViewById(R.id.btnRegister);
+        btnRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String username = edtUsername.getText().toString();
+                String password = edtPassword.getText().toString();
+                String email = edtEmail.getText().toString();
+                String passwordRetyped = edtRetypePassword.getText().toString();
+                try {
+                    JSONObject json_register = new JSONObject();
+                    json_register.put("username", username);
+                    json_register.put("password", password);
+                    json_register.put("email", email);
+                    if (password.equals(passwordRetyped)) {
+                        String response = requestHTTP(baseUrl + "/register", "post", json_register);
+                        if (response.length() != 0) {
+                            Intent intent = new Intent(SignupActivity.this, HomeActivity.class);
+                            startActivity(intent);
+                            finish(); 
+                        } else {
+                            Toast.makeText(SignupActivity.this, "Connection with server has failed", Toast.LENGTH_SHORT).show();
+                        }
+                    } else {
+                        Toast.makeText(SignupActivity.this, "Passwords don't match!", Toast.LENGTH_SHORT).show();
+                    }
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        });
     }
 }
