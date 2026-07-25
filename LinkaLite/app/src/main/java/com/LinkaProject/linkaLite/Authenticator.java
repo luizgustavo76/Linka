36ed416e779dbc600472645ac0c2980cf6213971
luizsgustavo76 <luizsgustavo76@gmail.com>
2026-07-25 14:45:41 -0300

    login nao esta mais quebrado

---
+++ LinkaLite/app/src/main/java/com/LinkaProject/linkaLite/Authenticator.java
@@ -0,0 +1,56 @@
+
+package com.LinkaProject.linkaLite;
+
+import android.accounts.AbstractAccountAuthenticator;
+import android.accounts.Account;
+import android.accounts.AccountAuthenticatorResponse;
+import android.accounts.NetworkErrorException;
+import android.content.Context;
+import android.os.Bundle;
+
+public class Authenticator extends AbstractAccountAuthenticator {
+
+    public Authenticator(Context context) {
+        super(context);
+    }
+
+    @Override
+    public Bundle addAccount(AccountAuthenticatorResponse response, String accountType, 
+                             String authTokenType, String[] requiredFeatures, Bundle options) 
+                             throws NetworkErrorException {
+        // Retornado quando o usuário clica em "Adicionar Conta" nas Configurações do Android
+        return null;
+    }
+
+    @Override
+    public Bundle editProperties(AccountAuthenticatorResponse response, String accountType) {
+        return null;
+    }
+
+    @Override
+    public Bundle confirmCredentials(AccountAuthenticatorResponse response, Account account, Bundle options) {
+        return null;
+    }
+
+    @Override
+    public Bundle getAuthToken(AccountAuthenticatorResponse response, Account account, 
+                               String authTokenType, Bundle options) {
+        return null;
+    }
+
+    @Override
+    public String getAuthTokenLabel(String authTokenType) {
+        return null;
+    }
+
+    @Override
+    public Bundle updateCredentials(AccountAuthenticatorResponse response, Account account, 
+                                     String authTokenType, Bundle options) {
+        return null;
+    }
+
+    @Override
+    public Bundle hasFeatures(AccountAuthenticatorResponse response, Account account, String[] features) {
+        return null;
+    }
+}
---
+++ LinkaLite/app/src/main/java/com/LinkaProject/linkaLite/AuthenticatorService.java
@@ -0,0 +1,24 @@
+package com.LinkaProject.linkaLite;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+
+public class AuthenticatorService extends Service {
+
+    private Authenticator mAuthenticator;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        mAuthenticator = new Authenticator(this);
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        if (intent.getAction().equals(android.accounts.AccountManager.ACTION_AUTHENTICATOR_INTENT)) {
+            return mAuthenticator.getIBinder();
+        }
+        return null;
+    }
+}
