5f26e1ee5a84d238d20e132504fce997daafb368
luizsgustavo76 <luizsgustavo76@gmail.com>
2026-07-27 14:13:44 -0300

    pagina de profiles ja iniciada

---
+++ LinkaLite/app/src/main/java/com/LinkaProject/linkaLite/profile.java
@@ -0,0 +1,18 @@
+package com.LinkaProject.linkaLite;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.Handler;
+import android.widget.ImageView;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+public class profile {
+    
+}
