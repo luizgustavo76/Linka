4d8495a47786afe097e3da4fa0a844a45ad437e9
luizsgustavo76 <luizsgustavo76@gmail.com>
2026-08-01 14:17:05 -0300

    adiçao de layouts de menus de federações

---
+++ LinkaLite/app/src/main/java/com/LinkaProject/linkaLite/feedFinder.java
@@ -0,0 +1,21 @@
+package com.LinkaProject.linkaLite;
+
+import android.app.Activity;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class feedFinder {
+    
+}
