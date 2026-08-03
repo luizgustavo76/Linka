09c90557b89263d03d5b39dd9ebe7434f1c878c7
luizsgustavo76 <luizsgustavo76@gmail.com>
2026-08-02 22:14:25 -0300

    ultimas alterações incoplemtas

---
+++ LinkaLite/app/src/main/java/com/LinkaProject/linkaLite/MessageNotifications.java
@@ -0,0 +1,27 @@
+public class MessageNotifications {
+    private int id;
+    private String content;
+    private String datetime;
+    private String fromUser;
+    private String receiver;
+    private int read;
+    private String type;
+
+    public Message(int id, String content, String datetime, String fromUser, String receiver, int read, String type) {
+        this.id = id;
+        this.content = content;
+        this.datetime = datetime;
+        this.fromUser = fromUser;
+        this.receiver = receiver;
+        this.read = read;
+        this.type = type;
+    }
+
+    public int getId() { return id; }
+    public String getContent() { return content; }
+    public String getDatetime() { return datetime; }
+    public String getFromUser() { return fromUser; }
+    public String getReceiver() { return receiver; }
+    public int getRead() { return read; }
+    public String getType() { return type; }
+}
