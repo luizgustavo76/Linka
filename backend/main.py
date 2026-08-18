6b86e49633299d74ba14de73906d33d6b3969e44
Luizs Gustavo <luizsgustavo76@gmail.com>
2026-08-18 20:17:55 -0300

    json pra selecionar os modulos que deseja que esteja no seu servidor

--- backend/main.py
+++ backend/main.py
@@ -1,35 +1,52 @@
+import json
+import os
+modules_flags = {}
+base_dir = os.path.dirname(os.path.abspath(__file__))
+json_path = os.path.join(base_dir, "backend.json")
+with open(json_path, "r") as f:
+    modules_flags = json.load(f)
+root_flags = modules_flags["modules-flags"]
 from flask_cors import CORS 
-from post import post_bp
-from chat import chat_bp
-from friends import friends_bp
+if root_flags["post"]:
+    from post import post_bp
+if root_flags["chat"]:
+    from chat import chat_bp
+if root_flags["friends"]:
+    from friends import friends_bp
 from login import login_bp
-from profiles import profile_bp
-from search import search_bp
-from status import status_bp
+if root_flags["profiles"]:
+    from profiles import profile_bp
+if root_flags["search"]:
+    from search import search_bp
+if root_flags["status"]:
+    from status import status_bp
 from meta import meta_bp
-from chat_global import chat_global_bp
-from federations import federations_bp
-from notifications import notifications_blueprint
-from images import image_bp
+if root_flags["chat_global"]:
+    from chat_global import chat_global_bp
+if root_flags["federations"]:
+    from federations import federations_bp
+if root_flags["notifications"]:
+    from notifications import notifications_blueprint
+if root_flags["images"]:
+    from images import image_bp
 from flask import Flask, Blueprint, request, jsonify, g
 import sqlite3
 import os
-from federation_index import federation_index_bp
-from sincronizer import sincronizer_bp
-from themes import theme_bp
+if root_flags["federation_index"]:
+    from federation_index import federation_index_bp
+if root_flags["sincronizer"]:
+    from sincronizer import sincronizer_bp
+if root_flags["themes"]:
+    from themes import theme_bp
 from datetime import datetime, timedelta
 from werkzeug.security import check_password_hash
 import secrets
 from chat_group import chat_group_bp
-base_dir = os.path.dirname(os.path.abspath(__file__))
 db_dir = os.path.join(base_dir, "DB")
 tokens_file = os.path.join(db_dir, "tokens.db")
 login_file = os.path.join(db_dir, "login.db")
 banned_file = os.path.join(db_dir, "banned.db")
 app = Flask(__name__)
-# =========================
-# BANCO DE DADOS
-# =========================
 def get_db():
     conn = sqlite3.connect(tokens_file)
     conn.row_factory = sqlite3.Row
@@ -75,9 +92,7 @@ create_db_banned()
 create_db()
 
 
-# =========================
-# FUNÇÕES
-# =========================
+
 
 def verificar_hash(senha, hash_salvo):
     return check_password_hash(hash_salvo, senha)
@@ -247,21 +262,35 @@ def valideManual():
 
 
 CORS(app, resources={r"/*": {"origins": "*"}})
-app.register_blueprint(status_bp)
-app.register_blueprint(search_bp)
-app.register_blueprint(profile_bp)
+if root_flags["status"]:
+    app.register_blueprint(status_bp)
+if root_flags["search"]:
+    app.register_blueprint(search_bp)
+if root_flags["profiles"]:
+    app.register_blueprint(profile_bp)
 app.register_blueprint(login_bp)
-app.register_blueprint(post_bp)
-app.register_blueprint(chat_bp)
-app.register_blueprint(sincronizer_bp)
-app.register_blueprint(friends_bp)
+if root_flags["post"]:
+    app.register_blueprint(post_bp)
+if root_flags["chat"]:
+    app.register_blueprint(chat_bp)
+if root_flags["sincronizer"]:
+    app.register_blueprint(sincronizer_bp)
+if root_flags["friends"]:
+    app.register_blueprint(friends_bp)
 app.register_blueprint(meta_bp)
-app.register_blueprint(chat_global_bp)
-app.register_blueprint(notifications_blueprint)
-app.register_blueprint(chat_group_bp)
-app.register_blueprint(image_bp)
-app.register_blueprint(theme_bp)
-app.register_blueprint(federations_bp)
-app.register_blueprint(federation_index_bp)
+if root_flags["chat_global"]:
+    app.register_blueprint(chat_global_bp)
+if root_flags["notifications"]:
+    app.register_blueprint(notifications_blueprint)
+if root_flags["chat_group"]:
+    app.register_blueprint(chat_group_bp)
+if root_flags["images"]:
+    app.register_blueprint(image_bp)
+if root_flags["themes"]:
+    app.register_blueprint(theme_bp)
+if root_flags["federations"]:
+    app.register_blueprint(federations_bp)
+if root_flags["federation_index"]:
+    app.register_blueprint(federation_index_bp)
 if __name__ == "__main__":
     app.run(host='0.0.0.0', debug=True, port=5000)
