c11272e43a2c423a29987b4b2ac241489934a054
luizgustavo76 <luizsgustavo76@gmail.com>
2026-08-27 21:34:04 -0300

    placeholder de pagina de editar a bio

---
+++ LinkaLite/app/src/main/java/com/LinkaProject/linkaLite/ChangeBiography.java
@@ -0,0 +1,12 @@
+package com.LinkaProject.linkaLite;
+import org.json.JSONObject;
+import org.json.JSONException;
+import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.Toast;
