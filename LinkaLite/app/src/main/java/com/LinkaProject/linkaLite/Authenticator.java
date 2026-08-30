7aaa19ce70513bc282ae88c135dd34b6c34e9690
luizgustavo76 <luizsgustavo76@gmail.com>
2026-08-29 22:29:35 -0300

    authenticator corrigido

--- LinkaLite/app/src/main/java/com/LinkaProject/linkaLite/Authenticator.java
+++ LinkaLite/app/src/main/java/com/LinkaProject/linkaLite/Authenticator.java
@@ -1,43 +1,66 @@
 package com.LinkaProject.linkaLite;
+
 import android.accounts.AbstractAccountAuthenticator;
 import android.accounts.Account;
 import android.accounts.AccountAuthenticatorResponse;
+import android.accounts.AccountManager;
 import android.accounts.NetworkErrorException;
 import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+
 public class Authenticator extends AbstractAccountAuthenticator {
+
+    private final Context context;
+
     public Authenticator(Context context) {
         super(context);
+        this.context = context;
     }
+
     @Override
     public Bundle addAccount(AccountAuthenticatorResponse response, String accountType, 
                              String authTokenType, String[] requiredFeatures, Bundle options) 
                              throws NetworkErrorException {
-        // Retornado quando o usuário clica em "Adicionar Conta" nas Configurações do Android
-        return null;
+        
+        // Cria a Intent para abrir a LoginActivity quando o usuário clicar em "Adicionar Conta" nas Configurações
+        Intent intent = new Intent(context, LoginActivity.class);
+        intent.putExtra(AccountManager.KEY_ACCOUNT_TYPE, accountType);
+        intent.putExtra(AccountManager.KEY_AUTHTOKEN, authTokenType);
+        intent.putExtra(AccountManager.KEY_ACCOUNT_AUTHENTICATOR_RESPONSE, response);
+
+        Bundle reply = new Bundle();
+        reply.putParcelable(AccountManager.KEY_INTENT, intent);
+        return reply;
     }
+
     @Override
     public Bundle editProperties(AccountAuthenticatorResponse response, String accountType) {
         return null;
     }
+
     @Override
     public Bundle confirmCredentials(AccountAuthenticatorResponse response, Account account, Bundle options) {
         return null;
     }
+
     @Override
     public Bundle getAuthToken(AccountAuthenticatorResponse response, Account account, 
                                String authTokenType, Bundle options) {
         return null;
     }
+
     @Override
     public String getAuthTokenLabel(String authTokenType) {
         return null;
     }
+
     @Override
     public Bundle updateCredentials(AccountAuthenticatorResponse response, Account account, 
                                      String authTokenType, Bundle options) {
         return null;
     }
+
     @Override
     public Bundle hasFeatures(AccountAuthenticatorResponse response, Account account, String[] features) {
         return null;
