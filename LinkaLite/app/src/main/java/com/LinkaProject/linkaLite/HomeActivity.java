1a566713cb68ae3cddda0ae1575ec873c15c0136
luizgustavo76 <luizsgustavo76@gmail.com>
2026-09-02 18:59:57 -0300

    otimização de feed usando threads

--- LinkaLite/app/src/main/java/com/LinkaProject/linkaLite/HomeActivity.java
+++ LinkaLite/app/src/main/java/com/LinkaProject/linkaLite/HomeActivity.java
@@ -256,40 +256,9 @@ public class HomeActivity extends Activity {
         }
     }
     public String requestHTTP(String urlParam, String method, JSONObject json_body) {
-        HttpURLConnection connection = null;
-        try {
-            URL url = new URL(urlParam);
-            connection = (HttpURLConnection) url.openConnection();
-            method = method.toUpperCase();
-            connection.setRequestMethod(method);
-            connection.setRequestProperty("Content-Type", "application/json");
-            if (method.equals("POST") || method.equals("PUT")) {
-                connection.setDoOutput(true);
-                OutputStream os = connection.getOutputStream();
-                os.write(json_body.toString().getBytes("UTF-8"));
-                os.flush();
-                os.close();
-            }
-            int responseCode = connection.getResponseCode();
-            if (responseCode == HttpURLConnection.HTTP_OK) {
-                BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream(), "UTF-8"));
-                StringBuilder response = new StringBuilder();
-                String line;
-                while ((line = in.readLine()) != null) {
-                    response.append(line);
-                }
-                in.close();
-                return response.toString();
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-        } finally {
-            if (connection != null) {
-                connection.disconnect();
-            }
-        }
-        return ""; 
-    }
+        String response = request.requestHTTP(urlParam, method, json_body, HomeActivity.this);
+        return response;
+    }   
     private ScheduledExecutorService scheduler;
     private ScheduledExecutorService schedulerNotifications;
     @Override
