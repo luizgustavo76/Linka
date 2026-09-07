5a146d975e5fc63d2f0ef0f5ec4dcb6599889747
luizgustavo76 <luizsgustavo76@gmail.com>
2026-09-07 14:15:58 -0300

    nova rota de verificar se há novas menções

---
+++ backend/mentions.py
@@ -0,0 +1,43 @@
+from flask import Blueprint, request, jsonify, g
+import sqlite3
+import os
+base_dir = os.path.dirname(os.path.abspath(__file__))
+db_dir = os.path.join(base_dir, "DB")
+mentions_bp = Blueprint("mentions_bp", __name__)
+def get_db():
+    conn = sqlite3.connect(db_dir + "/mentions.db")
+    conn.row_factory = sqlite3.Row
+    return conn
+def create_db():
+    conn = get_db()
+    cur = conn.cursor()
+    cur.execute("""CREATE TABLE IF NOT EXISTS mentions(
+    username TEXT,
+    author TEXT,
+    content TEXT,
+    type TEXT,
+    post_id INTEGER,
+    comment_id INTEGER)""")
+    conn.commit()
+    conn.close()
+create_db()
+@mentions_bp.route("/view-mentios", methods=["POST"])
+def view_mentios():
+    data = request.get_json()
+    username = data.get("username")
+    if username == g.username:
+        type = data.get("type")
+        conn = get_db()
+        cur = conn.cursor()
+        if type:
+            cur.execute("SELECT * FROM mentions WHERE username = ? AND type = ?", (username, type))
+            rows = cur.fetchall()
+            result = [dict(row) for row in rows]
+            return jsonify(result),200
+        else:
+            cur.execute("SELECT * FROM mentions WHERE username = ?", (username))
+            rows = cur.fetchall()
+            result = [dict(row) for row in rows]
+            return jsonify(result),200
+    else:
+        return jsonify({"status":"forbidden"}),200
