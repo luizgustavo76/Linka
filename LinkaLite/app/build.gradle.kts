392a233bc546dca3249c7ddb73facfba77245728
luizgustavo76 <luizsgustavo76@gmail.com>
2026-09-13 16:47:05 -0300

    ultimas alterações no linka desktop qt

--- LinkaLite/app/build.gradle.kts
+++ LinkaLite/app/build.gradle.kts
@@ -76,6 +76,14 @@ android {
             enableV2Signing = false
         }
     }
+    signingConfigs {
+        release {
+            storeFile file("chave-linka.jks") // caminho para onde salvou o arquivo
+            storePassword "Luiz08102012"
+            keyAlias "linka_key"
+            keyPassword "Luiz08102012"
+        }
+    }
     
     // CORREÇÃO 2: Removido o conflito do Java 8. 
     // Deixando o Java 17, o JDK 21 compila feliz e sem avisos obsoletos.
